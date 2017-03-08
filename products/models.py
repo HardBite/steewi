eba7618c58827946c618afe44880e3895c352b86
max <hardbytec0c0c0@gmail.com>
2017-03-08 15:05:33 +0200

    connects products app; adds product model; makes migration.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
