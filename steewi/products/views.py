146c4026724a0095223dadd1b88704e8dafd66b3
max <hardbytec0c0c0@gmail.com>
2017-03-09 15:19:17 +0200

    adds pagination and sorting capabilities in ListView.

--- steewi/products/views.py
+++ steewi/products/views.py
@@ -4,6 +4,18 @@ from .models import Product
 
 class ProductList(ListView):
     model = Product
+    paginate_by = 10
+
+    def get_queryset(self):
+        order = self.request.GET.get('order_by', 'name')
+        new_context = Product.objects.order_by(order)
+        return new_context
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductList, self).get_context_data(**kwargs)
+        context['order_by'] = self.request.GET.get('order_by', 'name')
+        context['ordering_options'] = ('name',)
+        return context
 
 class ProductDetail(DetailView):
     model = Product
