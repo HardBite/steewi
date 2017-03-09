0649dc49c88a8cbc30c02f1a3a20c369af56a3d2
max <hardbytec0c0c0@gmail.com>
2017-03-09 17:25:54 +0200

    adds and connects django-vote

--- config/settings/base.py
+++ config/settings/base.py
@@ -60,6 +60,7 @@ LOCAL_APPS = [
     'steewi.users.apps.UsersConfig',
     # Your stuff: custom apps go here
     'steewi.products',
+    'vote',
 ]
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
